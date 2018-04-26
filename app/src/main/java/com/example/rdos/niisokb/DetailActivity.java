9103210d73993b7f98264fc1b6f340500c8b6849
rdos <r_dos00@mail.ru>
2018-04-26 21:26:53 +0300

    DetailActivity.add DownloadImageTask

--- app/src/main/java/com/example/rdos/niisokb/DetailActivity.java
+++ app/src/main/java/com/example/rdos/niisokb/DetailActivity.java
@@ -1,20 +1,55 @@
 package com.example.rdos.niisokb;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
+
+import java.net.URL;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "ITEM_ID";
-    private int mItemId;
+    private int mPosition;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-        mItemId = getIntent().getIntExtra(EXTRA_POSITION, -1);
-        Log.i("DetailActivity", "mItemId=" + String.valueOf(mItemId));
+        mPosition = getIntent().getIntExtra(EXTRA_POSITION, -1);
+        Log.i("DetailActivity", "mPosition=" + String.valueOf(mPosition));
+        mImageView = (ImageView) findViewById(R.id.image_detail);
+        DownloadImageTask downloadImageTask = new DownloadImageTask(mImageView);
+        downloadImageTask.execute(App.restMan.getAndroidsImg(mPosition));
+//        mNetworkImageView.setImageURI(Uri.parse(App.restMan.getAndroidsImg(mPosition)));
+    }
 
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        private ImageView mImageView;
+
+        public DownloadImageTask(ImageView imageView) {
+            mImageView = imageView;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            Bitmap bmp = null;
+            try {
+                URL url = new URL(urls[0]);
+                bmp = BitmapFactory.decodeStream(url.openStream());
+            } catch (Exception e) {
+                //TODO: )))
+                e.printStackTrace();
+            }
+            return bmp;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            mImageView.setImageBitmap(result);
+        }
     }
 }
